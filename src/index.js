be9e236b2f2522462fe60b8ebcc2eadbc5b2fe1f
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-09 11:01:55 +0200

    orbit ready

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as OrbitControls from 'three-orbitcontrols';
 import { Scene } from 'three';
 import {cube} from './cube.js';
 import {line} from './line.js';
@@ -42,7 +43,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-cube.add(line);
+//cube.add(line);
 scene.add(cube);
 scene.add(light);
 scene.add(light2);
@@ -50,12 +51,17 @@ scene.add(light2.target);
 
 camera.position.z = 5;
 
+const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+//camera.position.set( 0, 20, 100 );
+controls.update();
 
 const animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.02;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.02;
+    //cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 
